a43d1c50784af1c76af918a70b15a761b5b170d6
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-02-24 14:46:18 +0000

    updated bio/skills

---
+++ script.js
@@ -0,0 +1,37 @@
+const colors = ["#809bce", "#95b8d1", "#b8e0d2", "#d6eadf", "#eac4d5", "#736ced"];
+const SVG_NS = 'http://www.w3.org/2000/svg';
+const SVG_XLINK = "http://www.w3.org/1999/xlink";
+
+let heartsRy = []
+
+function useTheHeart(n) {
+    let use = document.createElementNS(SVG_NS, 'use');
+    use.n = n;
+    use.setAttributeNS(SVG_XLINK, 'xlink:href', '#heart');
+    use.setAttributeNS(null, 'transform', `scale(${use.n})`);
+    use.setAttributeNS(null, 'fill', colors[n % colors.length]);
+    use.setAttributeNS(null, 'x', -69);
+    use.setAttributeNS(null, 'y', -69);
+    use.setAttributeNS(null, 'width', 138);
+    use.setAttributeNS(null, 'height', 138);
+
+    heartsRy.push(use)
+    hearts.appendChild(use);
+}
+
+for (let n = 18; n >= 0; n--) { useTheHeart(n) }
+
+function Frame() {
+    window.requestAnimationFrame(Frame);
+    for (let i = 0; i < heartsRy.length; i++) {
+        if (heartsRy[i].n < 18) {
+            heartsRy[i].n += .01
+        } else {
+            heartsRy[i].n = 0;
+            hearts.appendChild(heartsRy[i])
+        }
+        heartsRy[i].setAttributeNS(null, 'transform', `scale(${heartsRy[i].n})`);
+    }
+}
+
+Frame()
